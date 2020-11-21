68c15df04dffa9cfb1a5edd073b7ee5c18a13a95
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2020-11-21 19:51:49 +0800

    initial code

---
+++ Orion/Orion/OrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrionApp.swift
+//  Orion
+//
+//  Created by Renzhe Zheng on 2020/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
